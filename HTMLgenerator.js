27c8fa371908c9a5118e17351573a88add3350d8
Gurprem Bhullar <bhullargurprem@gmail.com>
2020-07-20 14:57:14 -0400

    create the HTMLgenerator.js file

--- HTMLgenerator.js
+++ HTMLgenerator.js
@@ -1,13 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+function HTMLgenerator(empInfo) {
+  console.log(empInfo)
+  return 
+  
+  `<!DOCTYPE html>
+  <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="development/Employee-Summary/style.css">
     <title>Employee Summary</title>
 </head>
+
 <body>
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
@@ -181,7 +186,11 @@
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
-    
+        crossorigin="anonymous"></script>    
 </body>
-</html>
+
+</html>`;
+
+}
+
+module.exports = HTMLgenerator
